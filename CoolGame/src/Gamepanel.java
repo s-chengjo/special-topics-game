c6e61c5781b1275b573fe6b8a62a1b5730061060
Jonathan <aybb@DANIEL-DESKTOP.lan>
2022-11-06 22:23:25 -0800

    Added apple class, need to make going over apple add length and leave

--- CoolGame/src/Gamepanel.java
+++ CoolGame/src/Gamepanel.java
@@ -4,6 +4,7 @@ import java.awt.Graphics;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.util.ArrayList;
+import java.util.Random;
 
 import javax.swing.JPanel;
 
@@ -17,8 +18,15 @@ public class Gamepanel extends JPanel implements Runnable, KeyListener {
 	
 	private BodyPart b;
 	private ArrayList<BodyPart> snake;
+	
+	private Apple apple;
+	private ArrayList<Apple> apples;
+	private Random r;
+	
 	private int xCoor = 10, yCoor = 10, size = 5;
 	private int ticks = 0;
+	
+	
 	
 	public Gamepanel() {
 		
@@ -27,6 +35,8 @@ public class Gamepanel extends JPanel implements Runnable, KeyListener {
 		addKeyListener(this);
 		
 		snake = new ArrayList<BodyPart>();
+		apples = new ArrayList<Apple>();
+		r = new Random();
 		
 		start();
 	}
@@ -69,6 +79,14 @@ public class Gamepanel extends JPanel implements Runnable, KeyListener {
 				snake.remove(0);
 			}
 		}
+		
+		if (apples.size() == 0) {
+			int xCoor = r.nextInt(49);
+			int yCoor = r.nextInt(49);
+			
+			apple = new Apple(xCoor, yCoor, 10);
+			apples.add(apple);
+		}
 	}
 	
 	public void paint(Graphics g) {
@@ -92,6 +110,10 @@ public class Gamepanel extends JPanel implements Runnable, KeyListener {
 		for (int i = 0; i <snake.size(); i++)
 		{
 			snake.get(i).draw(g);
+		}
+		
+		for (int i = 0; i < apples.size(); i++) {
+			apples.get(i).draw(g);
 		}
 	}
 	
