784dc4b764f981c2c37ccbaf780f2d61217fc8f7
Ariel <aybb@DANIEL-DESKTOP.lan>
2022-11-14 23:57:59 -0800

    Added a kirby sprite with png files
    also added more to player class

--- CoolGame/src/entity/Entity.java
+++ CoolGame/src/entity/Entity.java
@@ -1,8 +1,13 @@
 package entity;
 
+import java.awt.image.BufferedImage;
+
 public class Entity {
 	
 	public int x, y;
-	public int speed; 
+	public int speed;
+	
+	public BufferedImage one, two, three, four, five, six, seven, eight, nine, ten, eleven, twelve;
+	public String direction; 
 
 }
--- CoolGame/src/entity/Player.java
+++ CoolGame/src/entity/Player.java
@@ -1,9 +1,45 @@
 package entity;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
 import main.Gamepanel;
 
-public class Player {
+public class Player extends Entity{
 	
 	Gamepanel gp;
 	
+	public Player(Gamepanel gp) {
+		
+		this.gp = gp;
+		setDefaultValues();
+		getPlayerImage(); 
+		
+	}
 
+	public void setDefaultValues() {
+		x = 100;
+		y = 100;
+		speed = 1000;
+	}
+	
+	public void getPlayerImage() {
+		try {
+			
+		one = ImageIO.read(getClass().getResourceAsStream("/player/sprite_00.png")); 
+		two = ImageIO.read(getClass().getResourceAsStream("/player/sprite_01.png")); 
+		three = ImageIO.read(getClass().getResourceAsStream("/player/sprite_02.png")); 
+		four = ImageIO.read(getClass().getResourceAsStream("/player/sprite_03.png")); 
+		five = ImageIO.read(getClass().getResourceAsStream("/player/sprite_04.png")); 
+		six= ImageIO.read(getClass().getResourceAsStream("/player/sprite_05.png")); 
+		seven = ImageIO.read(getClass().getResourceAsStream("/player/sprite_06.png")); 
+		eight = ImageIO.read(getClass().getResourceAsStream("/player/sprite_07.png")); 
+		nine = ImageIO.read(getClass().getResourceAsStream("/player/sprite_08.png")); 
+		ten = ImageIO.read(getClass().getResourceAsStream("/player/sprite_09.png")); 
+		eleven = ImageIO.read(getClass().getResourceAsStream("/player/sprite_10.png")); 
+		twelve = ImageIO.read(getClass().getResourceAsStream("/player/sprite_11.png")); 
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 }
