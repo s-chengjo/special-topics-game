a7a5d9bbb082aa93c606685032149793654c3fdd
s-chengjo <s-chengjo@49S500119977.it.bsd405.org>
2023-02-27 20:46:59 -0800

    Added initial parts for evilbodypart class

--- CoolGame/src/main/EvilBodyPart.java
+++ CoolGame/src/main/EvilBodyPart.java
@@ -2,5 +2,17 @@ package main;
 
 public class EvilBodyPart {
 	
+	private int xCoor, yCoor, width, height;
+	
+	public EvilBodyPart(int xCoor, int yCoor, int tileSize) {
+		this.xCoor = xCoor;
+		this.yCoor = yCoor;
+		width = tileSize;
+		height = tileSize;
+	}
+	
+	public void tick() {
+		
+	}
 
 }
