7f118c37a1dc3ca9dbd9d057eb6fa21080718390
s-changar <s-changar@49S500119977.it.bsd405.org>
2023-05-28 22:24:51 -0700

    Cleaned up all code
    much more readable now
    added lots of comments and removed unnecessary code
    made more effecient

--- CoolGame/src/main/Gamepanel.java
+++ CoolGame/src/main/Gamepanel.java
@@ -18,45 +18,48 @@ import javax.imageio.ImageIO;
 
 public class Gamepanel extends JPanel implements Runnable, KeyListener {
 	
+	//Runnable variables
 	private static final long serialVersionUID = 1L;
-	public static int WIDTH = 0, HEIGHT = 0;
 	private Thread thread;
 	private boolean running;
+	private int totalTickrate;
+	
+	//Direction Variables
 	private boolean right = true, left = false, up = false, down = false;
 	private boolean right2 = true, left2 = false, up2 = false, down2 = false;
 	
-	private BodyPart b;
-	private ArrayList<BodyPart> snake;
-	
-	private BodyPart b2;
-	private ArrayList<BodyPart> snake2;
-	
-	private Apple apple;
-	private ArrayList<Apple> apples;
-	private Random appleRand;
-	
-	private Block block;
-	private ArrayList<Block> blocks;
-	private Random blockRand;
-	
+	//Width height and coordinates
+	public static int WIDTH = 0, HEIGHT = 0;
 	private int xCoor = 10, yCoor = 10, size = 5;
 	private int xCoor2 = 10, yCoor2 = 20;
 	private int ticks = 0;
 	
+	//Snake Body parts
+	private BodyPart b;
+	private ArrayList<BodyPart> snake;
+	private BodyPart b2;
+	private ArrayList<BodyPart> snake2;
 	
+	//Apples + Blocks
+	private Apple apple;
+	private ArrayList<Apple> apples;
+	private Random appleRand;
+	private Block block;
+	private ArrayList<Block> blocks;
+	private Random blockRand;
+	
+	//Cannon ball
 	private ArrayList<EvilBodyPart> CannonBall;
 	private EvilBodyPart cannonBall;
-	
-	private JFrame f;
-	private JLabel l;
-	private ImageIcon i;
-	private JPanel p;
-	
+
+	//Track of Scores
 	private ArrayList<Integer> Scores;
+	
+	//Two player variables
 	private int winThreshold;
 	private boolean invulnerable;
 	
-	private int totalTickrate;
+	//Images
 	private Image kirby;
 	private Image deathImg;
 	private Image TitleScreen;
@@ -65,7 +68,7 @@ public class Gamepanel extends JPanel implements Runnable, KeyListener {
 	private Image congratsImg;
 	
 	
-	//.
+	
 	public Gamepanel() {
 		
 		
@@ -80,6 +83,8 @@ public class Gamepanel extends JPanel implements Runnable, KeyListener {
 		invulnerable = false;
 		totalTickrate = 650000;
 		
+		
+		//Screen Size question
 		Scanner sc = new Scanner(System.in);
 		System.out.println("What size screen would you like to play on? (s, m, l)");
 		while (WIDTH == 0) {
@@ -101,50 +106,25 @@ public class Gamepanel extends JPanel implements Runnable, KeyListener {
 			}
 		}
 		
+		//Setting Gamepanel variables
 		setFocusable(true);
 		setPreferredSize(new Dimension(WIDTH, HEIGHT));
 		addKeyListener(this);
 		
-		
+		//Setting Images
 		try {
-		kirby = ImageIO.read(getClass().getResourceAsStream("/resources/sprite_00.png"));
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		
-		try {
+			kirby = ImageIO.read(getClass().getResourceAsStream("/resources/sprite_00.png"));
 			deathImg = ImageIO.read(getClass().getResourceAsStream("/resources/adjusted death img.png"));
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		
-		try {
 			TitleScreen = ImageIO.read(getClass().getResourceAsStream("/resources/kirby title screen.png"));
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		
-		try {
 			blockImg = ImageIO.read(getClass().getResourceAsStream("/resources/newcrate.png"));
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		
-		try {
 			appleImg = ImageIO.read(getClass().getResourceAsStream("/resources/tomato.png"));
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		
-		
-		try {
 			congratsImg = ImageIO.read(getClass().getResourceAsStream("/resources/tomato.png"));
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		
 		
-		
+		//Single vs Player question
 		System.out.println("Single player or double? (single, double)");
 		String playerInput = sc.next();
 		if (playerInput.equals("double")) {
@@ -159,6 +139,7 @@ public class Gamepanel extends JPanel implements Runnable, KeyListener {
 		}
 		
 		
+		//Double player question
 		if (snake2 != null) {
 			do {
 				int num = 0;
@@ -182,21 +163,19 @@ public class Gamepanel extends JPanel implements Runnable, KeyListener {
 			} while(winThreshold == 0);
 			totalTickrate = 450000;
 		}
-		
-		
-		
-		
-		//start();
-		
 	}
-	//
+	
+	
+	//Start game
 	public void start() {
 		running = true;
 		thread = new Thread(this);
 		thread.start();
 	}
 	
+	//Restart game
 	public void restart() {
+		
 		running = false;
 		System.out.println("You scored " + (size*100 - 500) + " points!");
 		Scores.add(size*100 - 500);
@@ -224,6 +203,7 @@ public class Gamepanel extends JPanel implements Runnable, KeyListener {
 		ticks = 0;
 	}
 	
+	//Ends game
 	public void stop() {
 		running = false;
 		System.out.println("You scored " + (size*100 - 500) + " points!");
@@ -236,7 +216,7 @@ public class Gamepanel extends JPanel implements Runnable, KeyListener {
 			}
 		}
 		
-		
+		//Result based on high score
 		System.out.println("Snake is dead now");
 		System.out.println("\nHere are some of your statistics:");
 		if (highscore > 0) {
@@ -252,6 +232,7 @@ public class Gamepanel extends JPanel implements Runnable, KeyListener {
 			//g.drawImage(congratsImg, 0, 0, null);
 		}
 		
+		//Average Score
 		int totalScore = 0;
 		for (int i = 0; i < Scores.size(); i++) {
 			totalScore += Scores.get(i);
@@ -259,6 +240,7 @@ public class Gamepanel extends JPanel implements Runnable, KeyListener {
 		System.out.println("\nYour average score was " + (totalScore / Scores.size()) + " points.\n");
 		
 		
+		//Lowest Score
 		int lowestScore = Scores.get(0);
 		for (int i = 1; i < Scores.size(); i++) {
 			if (Scores.get(i) < lowestScore) {
@@ -271,7 +253,7 @@ public class Gamepanel extends JPanel implements Runnable, KeyListener {
 			System.out.println("\nYour lowest score was " + lowestScore + " points.");
 		}
 		
-		
+		//Percentage of low score of high score
 		double percentOfHS = (double) lowestScore/highscore * 100;
 		if (percentOfHS == 1) {
 			System.out.println("We hope your single playthrough was fun and exciting!");
@@ -280,6 +262,7 @@ public class Gamepanel extends JPanel implements Runnable, KeyListener {
 		}
 	}
 	
+	//Double player End Game
 	public void stop2() {
 		
 		running = false; 
@@ -294,8 +277,10 @@ public class Gamepanel extends JPanel implements Runnable, KeyListener {
 		System.out.println("Kirpy won " + wins + " times and lost " + (Scores.size() - wins) + " times!");
 	}
 	
-	//ffwa
+	//Method runs every tick
 	public void tick() {
+		
+		//Set up Snake
 		if (snake.size() == 0)
 		{
 			b = new BodyPart(xCoor, yCoor, 10);
@@ -309,26 +294,7 @@ public class Gamepanel extends JPanel implements Runnable, KeyListener {
 		}
 		ticks++;
 		
-		if (CannonBall.size() > 0 == true && ticks % (totalTickrate*0.35) == 0) {
-			for (int i = 0; i < CannonBall.size(); i++) {
-				String direction = CannonBall.get(i).getDirection();
-				switch (direction) {
-				case "right":
-					CannonBall.get(i).moveRight();
-					break;
-				case "left":
-					CannonBall.get(i).moveLeft();
-					break;
-				case "up":
-					CannonBall.get(i).moveUp();
-					break;
-				case "down":
-					CannonBall.get(i).moveDown();
-					break;
-				}
-			}
-		}
-		
+		//Movement
 		if (ticks % totalTickrate == 0) {
 			if(right) xCoor++;
 			if(left) xCoor--;
@@ -360,8 +326,9 @@ public class Gamepanel extends JPanel implements Runnable, KeyListener {
 			
 		}
 		
+		//Shoots cannon ball every 10 frames
 		if (snake2 != null) {
-			if (ticks % (totalTickrate*10) == 0) { //every 10 frame s
+			if (ticks % (totalTickrate*10) == 0) {
 				if (right2 == true) {
 					cannonBall = new EvilBodyPart(xCoor2, yCoor2, 10, "right");
 				} else if (left2 == true) {
@@ -375,17 +342,18 @@ public class Gamepanel extends JPanel implements Runnable, KeyListener {
 			}
 		}
 		
+		//Removes invulnerability after 10 frames
 		if (invulnerable == true && ticks > totalTickrate*10) {
 			invulnerable = false;
 		}
 		
-		if (blocks.size() > 15) //Max 15 blocks at a time
+		//Max of 15 blocks at a time
+		if (blocks.size() > 15) 
 		{
 			blocks.remove(0);
 		}
 		
-		//Scrapped, ticks reset to 0 above, will leave for future use
-		
+		//Creates apples and blocks
 		if (apples.size() == 0) {
 			int xCoor = appleRand.nextInt(WIDTH/10 - 1);
 			int yCoor = appleRand.nextInt(HEIGHT/10 - 1);
@@ -400,8 +368,29 @@ public class Gamepanel extends JPanel implements Runnable, KeyListener {
 			blocks.add(block);
 		}
 		
+		//Cannonball movement 
+		if (CannonBall.size() > 0 == true && ticks % (totalTickrate*0.35) == 0) {
+			for (int i = 0; i < CannonBall.size(); i++) {
+				String direction = CannonBall.get(i).getDirection();
+				switch (direction) {
+				case "right":
+					CannonBall.get(i).moveRight();
+					break;
+				case "left":
+					CannonBall.get(i).moveLeft();
+					break;
+				case "up":
+					CannonBall.get(i).moveUp();
+					break;
+				case "down":
+					CannonBall.get(i).moveDown();
+					break;
+				}
+			}
+		}
 		
 		
+		//Eating apples
 		for (int i = 0; i < apples.size(); i++) {
 			if (xCoor == apples.get(i).getxCoor() && yCoor == apples.get(i).getyCoor()) {
 				size++;
@@ -415,8 +404,10 @@ public class Gamepanel extends JPanel implements Runnable, KeyListener {
 			}
 		}
 		
-		//blocks:
+		//DEATHS:
 		
+		
+		//By Block
 		for (int i = 0; i < blocks.size(); i++)
 		{
 			if (xCoor == blocks.get(i).getxCoor() && yCoor == blocks.get(i).getyCoor())
@@ -425,6 +416,7 @@ public class Gamepanel extends JPanel implements Runnable, KeyListener {
 				restart();
 			}
 		}
+		
 		
 		if (snake2 != null) {
 			for (int i = 0; i < blocks.size(); i++)
@@ -437,7 +429,7 @@ public class Gamepanel extends JPanel implements Runnable, KeyListener {
 			}
 		}
 		
-		
+		//By running into yourself
 		for (int i = 0; i < snake.size(); i++) {
 			if (xCoor == snake.get(i).getxCoor() && yCoor == snake.get(i).getyCoor()) {
 				if (i != snake.size() - 1) {
@@ -447,6 +439,18 @@ public class Gamepanel extends JPanel implements Runnable, KeyListener {
 			}
 		}
 		
+		//By running out of bounds
+		if (xCoor < 0 || xCoor > WIDTH/10 - 1 || yCoor < 0 || yCoor > HEIGHT/10 - 1) {
+			System.out.println("Game Over (snake out of bounds)");
+			restart();
+		}
+		
+		
+		
+		//Double player deaths:
+		
+		
+		//Schmirpy runs into himself
 		if (snake2 != null) {
 			for (int i = 0; i < snake2.size(); i++) {
 				if (xCoor2 == snake2.get(i).getxCoor() && yCoor2 == snake2.get(i).getyCoor()) {
@@ -458,18 +462,18 @@ public class Gamepanel extends JPanel implements Runnable, KeyListener {
 			}
 		}
 		
-		if (xCoor < 0 || xCoor > WIDTH/10 - 1 || yCoor < 0 || yCoor > HEIGHT/10 - 1) {
-			System.out.println("Game Over (snake out of bounds)");
-			restart();
-		}
+		//Schmirpy runs out of bounds
 		if (snake2 != null) { 
 			if (xCoor2 < 0 || xCoor2 > WIDTH/10 - 1 || yCoor2 < 0 || yCoor2 > HEIGHT/10 - 1) {
 				System.out.println("Game Over (snake2 out of bounds)");
 				restart();
 			}
 		}
+		
+		//Checks invulnerability
 		if (invulnerable == false)
 		{
+			//Getting caught by Schmirpy 
 		if (snake2 != null) {
 			for (int i = 0; i < snake.size(); i++) {
 				if (xCoor2 == snake.get(i).getxCoor() && yCoor2 == snake.get(i).getyCoor())
@@ -480,6 +484,7 @@ public class Gamepanel extends JPanel implements Runnable, KeyListener {
 			}
 		}
 		
+		//Cannonball kills Kirpy
 		if (snake2 != null) {
 			for (int i = 0; i < CannonBall.size(); i++) {
 				int x = CannonBall.get(i).getxCoor();
@@ -495,12 +500,13 @@ public class Gamepanel extends JPanel implements Runnable, KeyListener {
 		}
 	}
 	}
-	//draw
 	
+	//Drawing method
 	public void paint(Graphics g) {
 		
 		g.clearRect(0, 0, WIDTH, HEIGHT);
 		
+		//Background for multiplayer vs singleplayer
 		if (snake2!=null) {
 			g.setColor(Color.DARK_GRAY);
 		} else {
@@ -509,65 +515,69 @@ public class Gamepanel extends JPanel implements Runnable, KeyListener {
 		
 		g.fillRect(0, 0, WIDTH, HEIGHT);
 		
+		
 		if (snake2!=null) {
 			g.setColor(Color.YELLOW);
 		} else {
 			g.setColor(Color.BLUE);	
 		}
-		for (int i = 0; i < WIDTH/10; i++) //Draws lines going up (trying to make a grid) the 10 is by how much you want to increment the lines (spacing)
+		
+		//Draws lines going up (trying to make a grid) the 10 is by how much you want to increment the lines (spacing)
+		for (int i = 0; i < WIDTH/10; i++) 
 		{
 			g.drawLine(i*10, 0, i*10, HEIGHT);
 		}
 		
-		for (int i = 0; i < HEIGHT/10; i++) //Draws lines going sideways finishing the grid
+		//Draws lines going sideways finishing the grid
+		for (int i = 0; i < HEIGHT/10; i++) 
 		{
 			g.drawLine(0, i*10, WIDTH, i*10);
 		}
 		
+		//Draws Kirby
 		for (int i = 0; i <snake.size(); i++)
 		{
 			snake.get(i).draw(g, kirby);
 		}
 		
+		//Draws Schmirpy 
 		if (snake2 != null) {
 			for (int i = 0; i < snake2.size(); i++) {
 				snake2.get(i).drawEvil(g);
 			}
+			//Draws Cannonballs
 			for (int i = 0; i < CannonBall.size(); i++) {
 				CannonBall.get(i).draw(g);
 			}
 		}
 		
-		
+		//Draws apples
 		for (int i = 0; i < apples.size(); i++) {
 			apples.get(i).drawImg(g, appleImg);
 		} 
 		
+		//Draws Blocks
 		for (int i = 0; i < blocks.size(); i++) {
 			blocks.get(i).drawImg(g, blockImg);
 		}
 		
-		/*if (running == false) {
-			myPrintMethod(g);
-		}*/
-		
-		
+		//Draws title screen
 		if (Scores.size() == 0 && running == false)
 		{
 			g.clearRect(0, 0, WIDTH, HEIGHT);
 			g.drawImage(TitleScreen, 0, 0, null);
 		}
 		
+		//Draws death screen
 		if (Scores.size() > 0 && running == false)
 		{
 			g.clearRect(0, 0, WIDTH, HEIGHT);
 			g.drawImage(deathImg, 0, 0, null);
 		}
-		
-		
 	}
 	
 
+	//Run method
 	public void run() {
 		
 		while (running)
@@ -583,9 +593,10 @@ public class Gamepanel extends JPanel implements Runnable, KeyListener {
 	}
 
 	@Override
+	
 	//movement, takes input and changes direction
 	public void keyPressed(KeyEvent e) {
-		// TODO Auto-generated method stub
+		
 		int key = e.getKeyCode();
 		if (key == KeyEvent.VK_RIGHT && !left) {
 			right = true;
@@ -615,12 +626,10 @@ public class Gamepanel extends JPanel implements Runnable, KeyListener {
 			right = true;
 			up = false;
 			down = false;
-			f.setVisible(false);
 			start();
 		}
 		
 		if (key == KeyEvent.VK_UP && running == false) {
-			f.setVisible(false);
 			int highscore = 0;
 			for (int i = 0; i < Scores.size(); i++) {
 				if (Scores.get(i) > highscore) {
@@ -667,9 +676,9 @@ public class Gamepanel extends JPanel implements Runnable, KeyListener {
 
 	@Override
 	public void keyReleased(KeyEvent e) {
-		// TODO Auto-generated method stub
+		
 		
 	}
 }
-// nothinggg
+
 
